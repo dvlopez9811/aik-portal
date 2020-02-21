9c525559ada2ac5182d8e1ef7c52202fb312f745
dvlopez9811 <49213965+dvlopez9811@users.noreply.github.com>
2020-02-20 19:50:01 -0500

    Featuredbconnection (#2)
    * Creating branch featuredbconnection
    
    * addign mysql connection backend (not working yet)
    
    * Completing DB connection and bringing one table from it in buycars endpoint

--- aik-app-api/server.js
+++ aik-app-api/server.js
@@ -2,11 +2,7 @@
 var express = require('express');
 var app = express();
 var mysql = require("mysql");
-//const connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || '192.168.130.194:3000',
-//  user     : process.env.DB_USER || 'root',
-//  password : process.env.DB_PASS || 'password',
-//  database : process.env.DB_NAME || 'dbAIK'
+
 const con = mysql.createConnection({
   host: '192.168.130.194',
   user: 'root',
@@ -33,32 +29,17 @@ app.get('/', function(req, res){
   res.json(response);
 })
 
-// Implement the movies API endpoint
+// Implement the vehicles API endpoint
 app.get('/buycars', function(req, res){
- con.query('SELECT * FROM vehicles', (err,rows) => {
-    if(err) throw err;
-    console.log('Data received from Db:');
-    console.log(rows);
-  });
-/*con.query('SELECT JSON_ARRAYAGG(JSON_OBJECT('title', title)) FROM vehicles;', (err,rows) => {
-    if(err) throw err;
-    console.log('Data received from Db:');
-    console.log(rows);
-  });
-*/
-  var vehicles = [
-    {title : 'Xerato', release: '2020', score: 8, price: '80.000.000', description : 'Modern Car 1.6CC'},    
-    {title : 'Pikanto', release : '2020', score: 6, price: '40.000.000', description : 'Modern Car 1.0CC'},
-    {title : 'Rio Zedan', release: '2016', score: 9, price: '30.000.000', description : 'Modern Car 2.0CC'},
-    {title : 'Zoluto', release: '2016', score: 9, price: '20.000.000', description : 'Modern Car 3.0CC'},
-    {title : 'Stringer', release : '2015', score: 7, price: '10.000.000', description: 'Modern Car 3.0CC'},
-    {title : 'Ant-Man', release: '2015', score: 8, price: '70.000.000', description : 'Modern Car 1.0CC'},
-    {title : 'Rio Jatchbash', release : '2014', score: 10, price: '50.000.000', description : 'Modern Car 1.0CC'},
-  ]
-  
- res.json(vehicles);
-})
-
+  con.query('SELECT * FROM vehicles', (err,vehicles) => {
+     if(err) throw err;
+     console.log('Data received from Db:');
+     console.log(vehicles);
+     res.json(vehicles);
+   });
+ 
+ })
+ 
 //app.get('/', function(req, res, next) {   
     //now you can call the get-driver, passing a callback function
 //    getMovies(function (err, moviesResult){ 
@@ -70,6 +51,13 @@ app.get('/buycars', function(req, res){
 
 // Implement the reviewers API endpoint
 app.get('/vehicles', function(req, res){
+  con.query('SELECT * FROM vehicles', (err,vehicles) => {
+    if(err) throw err;
+    console.log('Data received from Db:');
+    console.log(vehicles);
+    res.json(vehicles);
+  });
+  /*
   var vehicles = [
     {name : 'Xerato', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/co/es/images/shoppingtool/Cerato-Showroom.png'},
     {name: 'Pikanto', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/co/es/images/showroom/PicantoNew/kia-co-picanto-ja.png'},
@@ -81,10 +69,18 @@ app.get('/vehicles', function(req, res){
   ];
 
   res.json(vehicles);
+*/
 })
 
 // Implement the publications API endpoint
 app.get('/support', function(req, res){
+  con.query('SELECT * FROM support', (err,support) => {
+    if(err) throw err;
+    console.log('Data received from Db:');
+    console.log(support);
+    res.json(support);
+  });
+  /*
   var support = [
     {name : 'Mechanics Appointments', avatar: 'glyphicon-eye-open'},
     {name : 'Post-sale service', avatar: 'glyphicon-fire'},
@@ -95,6 +91,7 @@ app.get('/support', function(req, res){
   ];
 
   res.json(support);
+*/
 })
 
 // Implement the pending reviews API endpoint
